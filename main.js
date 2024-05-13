acabcbe59939b25a7a300d3dd125dee2179d9d83
Jakub-Domogala <kuba.domogala@gmail.com>
2024-05-13 02:26:17 +0200

    add starting screen with demo

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 import { getApp, getPlayers, getStarSystem } from "./src/init_utils.js";
 import { GAME_TEMPO } from "./src/settings.js";
 
-(async () => {
-  // INIT APP
-  const app = await getApp();
-  const players = getPlayers(app);
-  const starSystem = await getStarSystem(app, players);
+// Function to hide the entry screen and start the game
+function startGame() {
+  const playerName = document.getElementById("playerName").value;
+  const gameDifficulty = parseInt(
+    document.getElementById("gameDifficulty").value,
+  );
 
-  // RUN GAME LOOP
-  let elapsed = 0.0;
-  app.ticker.add((ticker) => {
-    elapsed += ticker.deltaTime;
-    starSystem.update((ticker.deltaMS / 1000) * GAME_TEMPO);
-  });
-})();
+  document.getElementById("entryScreen").style.display = "none";
+  document.getElementById("gameContainer").style.display = "block";
+
+  // INIT APP
+  (async () => {
+    const app = await getApp();
+    const players = getPlayers(app);
+    const starSystem = await getStarSystem(
+      app,
+      players,
+      // playerName,
+      // gameDifficulty,
+    );
+
+    // RUN GAME LOOP
+    let elapsed = 0.0;
+    app.ticker.add((ticker) => {
+      elapsed += ticker.deltaTime;
+      starSystem.update((ticker.deltaMS / 1000) * GAME_TEMPO);
+    });
+  })();
+}
+
+// Event listener for the start button
+document.getElementById("startButton").addEventListener("click", startGame);
+// startGame();
