8fb9295a5ea6f775bf7851be23faa727228eb997
Jakub-Domogala <kuba.domogala@gmail.com>
2026-04-09 14:15:00 +0200

    Updates

---
+++ assets/batchSamplersUniformGroup-D6_nkOXV.js
@@ -0,0 +1,1 @@
+import{M as e}from"./index-DhX0eOWO.js";import{U as o}from"./colorToUniform-Bhm6v9yn.js";const t=new Int32Array(e);for(let r=0;r<e;r++)t[r]=r;const i=new o({uTextures:{value:t,type:"i32",size:e}},{isStatic:!0});export{i as b};
